a73d4d035956316a2bcba90ee20819b0661dafa4
Kevin Wang <wy721@qq.com>
2021-08-16 12:39:31 +0800

    enclave-api: Extract modules to files

---
+++ standalone/pruntime/enclave-api/src/actions.rs
@@ -0,0 +1,15 @@
+pub const ACTION_TEST: u8 = 0;
+pub const ACTION_INIT_RUNTIME: u8 = 1;
+pub const ACTION_GET_INFO: u8 = 2;
+pub const ACTION_DUMP_STATES: u8 = 3;
+pub const ACTION_LOAD_STATES: u8 = 4;
+// Reserved: 8, 9
+pub const ACTION_GET_RUNTIME_INFO: u8 = 10;
+pub const ACTION_GET_EGRESS_MESSAGES: u8 = 23;
+pub const ACTION_TEST_INK: u8 = 100;
+
+pub const BIN_ACTION_START: u8 = 128;
+pub const BIN_ACTION_SYNC_PARA_HEADER: u8 = BIN_ACTION_START + 0;
+pub const BIN_ACTION_DISPATCH_BLOCK: u8 = BIN_ACTION_START + 1;
+pub const BIN_ACTION_SYNC_HEADER: u8 = BIN_ACTION_START + 2;
+pub const BIN_ACTION_SYNC_COMBINED_HEADERS: u8 = BIN_ACTION_START + 3;
